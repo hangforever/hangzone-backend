4b33c918ba9ac0fd283a6f97ff2f72d6c3d6d8db
aburd <burdick.aaron@gmail.com>
2022-02-06 02:18:56 +0900

    messing around with rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+use rocket::request::Form;
+use rocket::State;
+use std::sync::Mutex;
+
+#[derive(FromForm)]
+struct Hanger {
+    id: usize,
+    name: String,
+}
+
+struct HangzoneState {
+    hit_count: Mutex<usize>,
+}
+
+#[get("/")]
+fn index(s: State<HangzoneState>) -> String {
+    let state = s.inner();
+    let mut count = state.hit_count.lock().unwrap();    
+    *count += 1; 
+    format!("Hello, hanger! This page has been visited {} times.", *count)
+}
+
+#[get("/?<hanger..>")]
+fn hanger(hanger: Form<Hanger>) -> String {
+    format!(
+        "The hanger's name is {} and their id is {}",
+        hanger.name, hanger.id
+    )
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .manage(HangzoneState {
+            hit_count: Mutex::new(0),
+        })
+        .mount("/", routes![index])
+        .mount("/hanger", routes![hanger])
+        .launch();
 }
