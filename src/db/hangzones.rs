11faaf1bf6be00a432daaf715350f7fd8392cce8
aburd <burdick.aaron@gmail.com>
2022-02-17 01:20:38 +0900

    fix: api

--- src/db/hangzones.rs
+++ src/db/hangzones.rs
@@ -3,44 +3,44 @@ use sqlx::postgres::PgPool;
 use sqlx::Row;
 
 pub async fn find_one(
-  pool: &PgPool,
-  slug: Option<&str>,
-  hangzone_id: Option<i32>,
+    pool: &PgPool,
+    slug: Option<&str>,
+    hangzone_id: Option<i32>,
 ) -> Option<HangzoneJson> {
-  if let Some(s) = slug {
-    let row = sqlx::query("SELECT * FROM hangzones WHERE slug = $1")
-      .bind(s)
-      .fetch_one(pool)
-      .await
-      .unwrap();
-    return Some(row_to_hangzone_json(row));
-  }
-  if let Some(h_id) = hangzone_id {
-    let row = sqlx::query("SELECT * FROM hangzones WHERE id = $1")
-      .bind(h_id)
-      .fetch_one(pool)
-      .await
-      .unwrap();
-    return Some(row_to_hangzone_json(row));
-  }
-  None
+    if let Some(s) = slug {
+        let row = sqlx::query("SELECT * FROM hangzones WHERE slug = $1")
+            .bind(s)
+            .fetch_one(pool)
+            .await
+            .unwrap();
+        return Some(row_to_hangzone_json(row));
+    }
+    if let Some(h_id) = hangzone_id {
+        let row = sqlx::query("SELECT * FROM hangzones WHERE id = $1")
+            .bind(h_id)
+            .fetch_one(pool)
+            .await
+            .unwrap();
+        return Some(row_to_hangzone_json(row));
+    }
+    None
 }
 
 fn row_to_hangzone_json(row: sqlx::postgres::PgRow) -> HangzoneJson {
-  HangzoneJson {
-    id: row.get("id"),
-    slug: row.get("slug"),
-    name: row.get("name"),
-    description: row.get("description"),
-    address_1: row.get("address_1"),
-    address_2: row.get("address_2"),
-    address_3: row.get("address_3"),
-    city: row.get("city"),
-    state: row.get("state"),
-    country: row.get("country"),
-    postal_code: row.get("postal_code"),
-    geography: row.get("geography"),
-    created_at: row.get("created_at"),
-    updated_at: row.get("updated_at"),
-  }
+    HangzoneJson {
+        id: row.get("id"),
+        slug: row.get("slug"),
+        name: row.get("name"),
+        description: row.get("description"),
+        address_1: row.get("address_1"),
+        address_2: row.get("address_2"),
+        address_3: row.get("address_3"),
+        city: row.get("city"),
+        state: row.get("state"),
+        country: row.get("country"),
+        postal_code: row.get("postal_code"),
+        // geography: row.get("geography"),
+        // created_at: row.get("created_at"),
+        // updated_at: row.get("updated_at"),
+    }
 }
--- src/routes/hangzones.rs
+++ src/routes/hangzones.rs
@@ -1,13 +1,15 @@
 use crate::db;
-use crate::models::hangzones::HangzoneJson;
-use rocket::serde::json::Json;
+use rocket::serde::json::{json, Value};
 use rocket::State;
 use sqlx::postgres::PgPool;
 
 #[get("/hangzones/<slug>")]
-pub async fn get_hangzone(slug: String, pool: &State<PgPool>) -> Json<HangzoneJson> {
-  let hangzone = db::hangzones::find_one(&pool.inner(), Some(&slug), None)
-    .await
-    .unwrap();
-  Json(hangzone)
+pub async fn get_hangzone(slug: String, pool: &State<PgPool>) -> Value {
+    let hangzone = db::hangzones::find_one(pool, Some(&slug), None).await;
+
+    if let Some(h) = hangzone {
+        json!({ "hangzone": h })
+    } else {
+        json!({ "hangzone": null })
+    }
 }
