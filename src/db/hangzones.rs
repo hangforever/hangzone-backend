f33c333b0d1897f4bcf77fc9fc13e62f4178cd2d
aburd <burdick.aaron@gmail.com>
2022-02-26 23:11:54 +0900

    add get hangzones by name

--- src/db/hangzones.rs
+++ src/db/hangzones.rs
@@ -11,17 +11,19 @@ pub async fn find_one(
         let row = sqlx::query("SELECT * FROM hangzones WHERE slug = $1")
             .bind(s)
             .fetch_one(pool)
-            .await
-            .unwrap();
-        return Some(row_to_hangzone_json(row));
+            .await;
+        if let Ok(r) = row {
+            return Some(row_to_hangzone_json(r));
+        }
     }
     if let Some(h_id) = hangzone_id {
         let row = sqlx::query("SELECT * FROM hangzones WHERE id = $1")
             .bind(h_id)
             .fetch_one(pool)
-            .await
-            .unwrap();
-        return Some(row_to_hangzone_json(row));
+            .await;
+        if let Ok(r) = row {
+            return Some(row_to_hangzone_json(r));
+        }
     }
     None
 }
@@ -32,15 +34,23 @@ pub struct FindHangzones {
     latlng: Option<(f32, f32)>,
 }
 
-pub async fn find(pool: &PgPool, params: FindHangzones) -> Vec<HangzoneJson> {
+pub async fn find(pool: &PgPool, params: FindHangzones) -> Option<Vec<HangzoneJson>> {
     // TODO: support GPS coordinates with latlng
 
-    sqlx::query("SELECT * FROM hangzones WHERE name ILIKE $1 || '%'")
-        .bind(params.search)
-        .map(|row| row_to_hangzone_json(row))
-        .fetch_all(pool)
-        .await
-        .unwrap()
+    if let Some(search) = params.search {
+        let hangzones = sqlx::query("SELECT * FROM hangzones WHERE name ILIKE $1 || '%'")
+            .bind(search)
+            .map(|row| row_to_hangzone_json(row))
+            .fetch_all(pool)
+            .await;
+        match hangzones {
+            Ok(hangzones) => {
+                return Some(hangzones);
+            }
+            Err(e) => eprintln!("{}", e),
+        }
+    }
+    None
 }
 
 fn row_to_hangzone_json(row: sqlx::postgres::PgRow) -> HangzoneJson {
@@ -53,9 +63,9 @@ fn row_to_hangzone_json(row: sqlx::postgres::PgRow) -> HangzoneJson {
         address_2: row.get("address_2"),
         address_3: row.get("address_3"),
         city: row.get("city"),
-        state: row.get("state"),
-        country: row.get("country"),
-        postal_code: row.get("postal_code"),
+        // state: row.get("state"),
+        // country: row.get("country"),
+        // postal_code: row.get("postal_code"),
         // geography: row.get("geography"),
         // created_at: row.get("created_at"),
         // updated_at: row.get("updated_at"),
