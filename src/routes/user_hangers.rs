cd3cc58412580773fa7b82b6cfa877fe9f413849
Aaron Burdick <burdick.aaron@gmail.com>
2022-03-20 13:25:15 +0900

    docs: user hangers

--- src/routes/user_hangers.rs
+++ src/routes/user_hangers.rs
@@ -8,30 +8,6 @@ use rocket::State;
 use sqlx::postgres::PgPool;
 use sqlx::FromRow;
 
-#[get("/users")]
-pub async fn get_user(auth: Auth, pool: &State<PgPool>, state: &State<AppState>) -> Value {
-    let user_hanger = db::user_hangers::find_one(pool, auth.id).await;
-    let secret = state.secret.clone();
-    if let Ok(u) = user_hanger {
-        return json!({ "user_hanger": u.to_user_auth(&secret) });
-    }
-    json!({ "user_hanger": null })
-}
-
-#[post("/users", data = "<user_body>")]
-pub async fn register_user(
-    user_body: Json<UserBody>,
-    pool: &State<PgPool>,
-    state: &State<AppState>,
-) -> Value {
-    let user_hanger = db::user_hangers::create_one(pool, user_body.into_inner()).await;
-    let secret = state.secret.clone();
-    if let Ok(u) = user_hanger {
-        return json!({ "user_hanger": u.to_user_auth(&secret) });
-    }
-    json!({ "user_hanger": null })
-}
-
 #[derive(Deserialize)]
 pub struct LoginUser {
     user_hanger: LoginUserData,
@@ -55,6 +31,30 @@ pub async fn post_login(
     let user_hanger = db::user_hangers::login(pool, &login_user.email, &login_user.password).await;
     if let Some(user_hanger) = user_hanger {
         return json!({ "user_hanger": user_hanger.to_user_auth(&secret) });
+    }
+    json!({ "user_hanger": null })
+}
+
+#[post("/users", data = "<user_body>")]
+pub async fn register_user(
+    user_body: Json<UserBody>,
+    pool: &State<PgPool>,
+    state: &State<AppState>,
+) -> Value {
+    let user_hanger = db::user_hangers::create_one(pool, user_body.into_inner()).await;
+    let secret = state.secret.clone();
+    if let Ok(u) = user_hanger {
+        return json!({ "user_hanger": u.to_user_auth(&secret) });
+    }
+    json!({ "user_hanger": null })
+}
+
+#[get("/users")]
+pub async fn get_user(auth: Auth, pool: &State<PgPool>, state: &State<AppState>) -> Value {
+    let user_hanger = db::user_hangers::find_one(pool, auth.id).await;
+    let secret = state.secret.clone();
+    if let Ok(u) = user_hanger {
+        return json!({ "user_hanger": u.to_user_auth(&secret) });
     }
     json!({ "user_hanger": null })
 }
